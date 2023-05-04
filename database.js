395c81039ece38e6595252ac7b52034ff2614f3a
minkman <lomick2090@gmail.com>
2023-05-04 10:46:36 -0500

    Create database.js

--- database.js
+++ database.js
@@ -1,8 +1,42 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
+}).promise();
 
-})
+
+async function getNotes() {
+    const result = await pool.query('SELECT * from note');
+    const rows = result[0]
+
+    return rows
+}
+
+async function getNoteByTitle(title) {
+    const [rows] = await pool.query(`
+        SELECT *
+        FROM note
+        WHERE title = ?
+    `, [title] )
+
+    return rows
+}
+
+async function createNote(title, description) {
+    const [result] = await pool.query(`
+    INSERT INTO note (title, description)
+    VALUES (?, ?)
+    `, [title, description])
+}
+
+const notes = await getNotes();
+const note = await getNoteByTitle('to-do')
+
+//console.log(notes)
+
+console.log(note)
